85786b463f94eef98001869477600a514f27e408
bratax <klintonjay.19@gmail.com>
2023-07-16 13:13:14 +0800

    redering templates

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login_view')
 def login():
-    return "<p> Login </p>"
+    return render_template("login.html")
 
 @auth.route('/logout_view')
 def logout():
@@ -12,4 +12,4 @@ def logout():
 
 @auth.route('/sign-up_view')
 def sign_up():
-    return "<p> sign Up </p>"
+    return render_template("sign-up.html")
