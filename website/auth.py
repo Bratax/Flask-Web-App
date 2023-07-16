84d8ca0439135dabee83dab6ae0cc72b5b79b72b
bratax <klintonjay.19@gmail.com>
2023-07-16 16:19:33 +0800

    creating post, request methods

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login_view')
+@auth.route('/login_view', methods=['GET', 'POST'])
 def login():
     return render_template("login.html", boolean=True)
 
@@ -10,6 +10,6 @@ def login():
 def logout():
     return "<p> logout </p>"
 
-@auth.route('/sign-up_view')
+@auth.route('/sign-up_view', methods=['GET', 'POST'])
 def sign_up():
     return render_template("sign-up.html")
